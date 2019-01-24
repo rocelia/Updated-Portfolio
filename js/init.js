892bdeb85cdcc755da90f83e15f7f52a019a258c
rocelia <you@example.com>
2019-01-24 16:46:49 -0500

    updated portfolio

---
+++ js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.dropdown-trigger');
+      var instances = M.Dropdown.init(elems, options);
+    });
+  
+    // Or with jQuery
+  
+    $('.dropdown-trigger').dropdown();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
